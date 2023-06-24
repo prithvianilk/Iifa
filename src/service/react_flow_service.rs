08c52641c76fbb28a39640d82d9e991c16318f95
prithvianilk <thepirateking3d2y@gmail.com>
2023-06-24 21:20:52 +0530

    Position tree based on node level

--- src/service/react_flow_service.rs
+++ src/service/react_flow_service.rs
@@ -2,7 +2,8 @@ use crate::service::dt_service::DtService;
 use crate::domain::node::Node;
 use crate::dto::react_flow_dtos::*;
 
-const STEP: f64 = 200.0;
+const X_STEP: f64 = 200.0;
+const Y_STEP: f64 = 100.0;
 
 #[derive(Clone)]
 pub struct ReactFlowService {
@@ -14,7 +15,8 @@ impl ReactFlowService {
         let root = self.dt_service.get_dt();
         let mut nodes: Vec<FlowNode> = Vec::new();
         let mut edges: Vec<FlowEdge>= Vec::new();
-        self.populate_nodes_and_edges(&root, 1, &Position { x: 0.0, y: 0.0 }, &mut nodes, &mut edges);
+        self.populate_nodes_and_edges(&root, 1, &mut nodes, &mut edges);
+        println!();
         Graph{nodes, edges}
     }
 
@@ -23,24 +25,30 @@ impl ReactFlowService {
         self.dt_service.save_dt(&root.unwrap())
     }
 
-    fn populate_nodes_and_edges(&self, root: &Node, id: u32, position: &Position, nodes: &mut Vec<FlowNode>, edges: &mut Vec<FlowEdge>) {
+    fn populate_nodes_and_edges(&self, root: &Node, id: u32, nodes: &mut Vec<FlowNode>, edges: &mut Vec<FlowEdge>) {
+        let upper_bound = closest_bigger_power_of_2(id as f64);
+        let index_at_level = id as f64 - upper_bound / 2.0;
+        let node_x_pos = (index_at_level - (upper_bound / 8.0)) * X_STEP; 
+        let node_y_pos = Y_STEP * upper_bound / 2.0;
+        let position = Position{x: node_x_pos, y: node_y_pos};
+
         let is_terminal = root.value.is_some();
         if is_terminal {
             let data = Data { label: root.value.clone().unwrap(), description: root.description.clone(), value: root.value.clone(), predicate: root.predicate.clone() };
-            nodes.push(FlowNode{id: id.to_string(), position: position.clone(), data});
+            nodes.push(FlowNode{id: id.to_string(), position, data});
             return
         }
         let data = Data{ label: root.description.clone(), description: root.description.clone(), value: root.value.clone(), predicate: root.predicate.clone() };
-        nodes.push(FlowNode{id: id.to_string(), position: position.clone(), data });
+        nodes.push(FlowNode{id: id.to_string(), position, data });
         if let Some(left) = &root.left {
             let edge_id = format!("{}-{}", id, id * 2);
             edges.push(FlowEdge { id: edge_id, source: id.to_string(), target: (id * 2).to_string(), label: Direction::LEFT });
-            self.populate_nodes_and_edges(left, id * 2, &Position { x: position.x - STEP, y: position.y + STEP }, nodes, edges)
+            self.populate_nodes_and_edges(left, id * 2, nodes, edges)
         }
         if let Some(right) = &root.right {
             let edge_id = format!("{}-{}", id, id * 2 + 1);
             edges.push(FlowEdge { id: edge_id, source: id.to_string(), target: (id * 2 + 1).to_string(), label: Direction::RIGHT });
-            self.populate_nodes_and_edges(right, id * 2 + 1, &Position { x: position.x + STEP, y: position.y + STEP }, nodes, edges)
+            self.populate_nodes_and_edges(right, id * 2 + 1, nodes, edges)
         }
     }
 
@@ -79,4 +87,15 @@ impl ReactFlowService {
     fn has_incoming_edge(&self, node: &FlowNode, edges: &[FlowEdge]) -> bool {
         edges.iter().any(|edge| edge.target == node.id)
     }
+}
+
+fn closest_bigger_power_of_2(value: f64) -> f64 {
+    let mut upper_bound = 1.0;
+    loop {
+        if value < upper_bound {
+            break;
+        }
+        upper_bound *= 2.0;
+    }
+    upper_bound
 }
